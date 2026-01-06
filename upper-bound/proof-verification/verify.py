eac1578656df99e3aeb45af997285ffdf2c6bc9b
noel-friedrich <noel.friedrich@outlook.de>
2026-01-06 16:58:35 +0100

    added rounded weights and verification script

---
+++ upper-bound/proof-verification/verify.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+# import and verify weights from disk
+weights = np.load("upper-bound/weights/rounded-weights.npy")
+assert weights.shape == (1189, 841) and weights.dtype == np.int64
+
+# assert that all valid rectangle placements (vertical + horizontal) have weight >= 1
+assert all(weights[i:i+26, j:j+37].sum() >= 1e6 for i in range(1189-26+1) for j in range(841-37+1))
+assert all(weights[i:i+37, j:j+26].sum() >= 1e6 for i in range(1189-37+1) for j in range(841-26+1))
+
+# assert the total weight sum is correct and nonnegative weights
+assert weights.min() >= 0 and weights.sum() < 1039e6
